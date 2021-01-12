f9c734286e9501c579ecd2e869cae13995fbe50f
Michael Neeley <micneeley14@gmail.com>
2021-01-12 15:39:46 -0500

    added amplitude

--- apolloschurchapp/src/Providers.js
+++ apolloschurchapp/src/Providers.js
@@ -7,8 +7,11 @@ import { NotificationsProvider } from '@apollosproject/ui-notifications';
 import { LiveProvider } from '@apollosproject/ui-connected';
 import { checkOnboardingStatusAndNavigate } from '@apollosproject/ui-onboarding';
 
+import RNAmplitude from 'react-native-amplitude-analytics';
 import ClientProvider, { client } from './client';
 import customTheme, { customIcons } from './theme';
+
+const amplitude = new RNAmplitude(ApollosConfig.AMPLITUDE_API_KEY);
 
 const AppProviders = (props) => (
   <ClientProvider {...props}>
@@ -26,7 +29,12 @@ const AppProviders = (props) => (
           })
         }
       >
-        <AnalyticsProvider>
+        <AnalyticsProvider
+          trackFunctions={[
+            ({ eventName, properties }) =>
+              amplitude.logEvent(eventName, properties),
+          ]}
+        >
           <LiveProvider>
             <Providers
               themeInput={customTheme}
