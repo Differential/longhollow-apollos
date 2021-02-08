73d85f49c4d12f89f079fd4a5270f8fbdcbcbc8d
Michael Neeley <micneeley14@gmail.com>
2021-02-08 09:58:38 -0500

    adds vimeo integration (#1)
    * adds vimeo integration
    
    * fix linter

--- apolloschurchapp/src/theme/icons/Discover.js
+++ apolloschurchapp/src/theme/icons/Discover.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Svg, { Path } from 'react-native-svg';
 import { makeIcon } from '@apollosproject/ui-kit';
 
-const Icon = makeIcon(({ size, fill, ...otherProps } = {}) => (
+const Icon = makeIcon(({ size, fill } = {}) => (
   <Svg width={size} height={size} viewBox="0 0 26 22" fill="none">
     <Path
       d="M13 4.625C13 3.69674 13.3687 2.8065 14.0251 2.15013C14.6815 1.49375 15.5717 1.125 16.5 1.125H23.5C23.7321 1.125 23.9546 1.21719 24.1187 1.38128C24.2828 1.54538 24.375 1.76794 24.375 2V16C24.375 16.2321 24.2828 16.4546 24.1187 16.6187C23.9546 16.7828 23.7321 16.875 23.5 16.875H16.5C15.5717 16.875 14.6815 17.2437 14.0251 17.9001C13.3687 18.5565 13 19.4467 13 20.375"
--- apolloschurchapp/src/theme/icons/Video.js
+++ apolloschurchapp/src/theme/icons/Video.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Svg, { Path } from 'react-native-svg';
 import { makeIcon } from '@apollosproject/ui-kit';
 
-const Icon = makeIcon(({ size, fill, ...otherProps } = {}) => (
+const Icon = makeIcon(({ size, fill } = {}) => (
   <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
     <Path
       d="M12 22.5C17.799 22.5 22.5 17.799 22.5 12C22.5 6.20101 17.799 1.5 12 1.5C6.20101 1.5 1.5 6.20101 1.5 12C1.5 17.799 6.20101 22.5 12 22.5Z"
