516488dc52b0213e2f675d334878f19a7878cc47
Michael Neeley <micneeley14@gmail.com>
2021-02-24 10:02:20 -0500

    new wordmark

--- apolloschurchapp/src/theme/propOverrides.js
+++ apolloschurchapp/src/theme/propOverrides.js
@@ -1,2 +1,13 @@
 /* Export your custom prop overrides here. */
-export {};
+
+export default {
+  // 'ui-kit.HighlightCard': {
+  // LabelComponent: () => () => null,
+  // },
+  // 'ui-kit.DefaultCard': {
+  // LabelComponent: () => () => null,
+  // },
+  // 'ui-kit.FeaturedCard': {
+  // LabelComponent: () => () => null,
+  // },
+};
