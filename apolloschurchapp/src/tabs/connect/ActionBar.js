f6c94785a4e3fad634e32b767a3407261fac688b
Michael Neeley <micneeley14@gmail.com>
2021-02-26 10:08:08 -0500

    wordmark and giving content

--- apolloschurchapp/src/tabs/connect/ActionBar.js
+++ apolloschurchapp/src/tabs/connect/ActionBar.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import { ActionBar, ActionBarItem } from '@apollosproject/ui-kit';
 import { RockAuthedWebBrowser } from '@apollosproject/ui-connected';
+import { useNavigation } from '@react-navigation/native';
 
-const Toolbar = () => (
-  <RockAuthedWebBrowser>
-    {(openUrl) => (
-      <ActionBar>
-        <ActionBarItem
-          onPress={() => openUrl('https://longhollow.online/groups/')}
-          icon="groups"
-          label="My Groups"
-        />
-        <ActionBarItem
-          onPress={() => openUrl('https://longhollow.online/give/')}
-          icon="heart-solid"
-          label="Give"
-        />
-        <ActionBarItem
-          onPress={() => openUrl('https://longhollow.online/help/')}
-          icon="question"
-          label="Get help"
-        />
-      </ActionBar>
-    )}
-  </RockAuthedWebBrowser>
-);
+const Toolbar = () => {
+  const navigation = useNavigation();
+  return (
+    <RockAuthedWebBrowser>
+      {(openUrl) => (
+        <ActionBar>
+          <ActionBarItem
+            onPress={() => openUrl('https://longhollow.online/groups/')}
+            icon="groups"
+            label="My Groups"
+          />
+          <ActionBarItem
+            onPress={() =>
+              navigation.navigate('NodeSingle', {
+                nodeId: 'UniversalContentItem:66e7a69ceb06c1ae1b8e792528813836',
+              })
+            }
+            icon="heart-solid"
+            label="Give"
+          />
+          <ActionBarItem
+            onPress={() => openUrl('https://longhollow.online/help/')}
+            icon="question"
+            label="Get help"
+          />
+        </ActionBar>
+      )}
+    </RockAuthedWebBrowser>
+  );
+};
 
 export default Toolbar;
