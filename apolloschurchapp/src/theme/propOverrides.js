b779b7529d73042d793ec384ec77445b4c5b6cc3
Michael Neeley <micneeley14@gmail.com>
2021-03-03 13:42:17 -0500

    prop overrides

--- apolloschurchapp/src/theme/propOverrides.js
+++ apolloschurchapp/src/theme/propOverrides.js
@@ -1,13 +1,13 @@
 /* Export your custom prop overrides here. */
 
 export default {
-  // 'ui-kit.HighlightCard': {
-  // labelText: '',
-  // },
-  // 'ui-kit.DefaultCard': {
-  // labelText: '',
-  // },
-  // 'ui-kit.FeaturedCard': {
-  // labelText: '',
-  // },
+  'ui-kit.HighlightCard': {
+    labelText: '',
+  },
+  'ui-kit.DefaultCard': {
+    labelText: '',
+  },
+  'ui-kit.FeaturedCard': {
+    labelText: '',
+  },
 };
