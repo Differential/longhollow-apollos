f0494e04c864e998af26820c847e3cb274c7487a
Michael Neeley <micneeley14@gmail.com>
2021-03-15 16:41:07 -0400

    add featured and subtitle fields (#3)

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -1,9 +1,27 @@
+import gql from 'graphql-tag';
 import { ContentItem } from '@apollosproject/data-connector-rock';
 
-const { resolver, schema, dataSource: CoreContentItem } = ContentItem;
+const schema = gql`
+  ${ContentItem.schema}
 
-class dataSource extends CoreContentItem {
+  extend type UniversalContentItem {
+    isFeatured: Boolean
+    subtitle: String
+  }
+`;
+
+class dataSource extends ContentItem.dataSource {
   attributeIsVideo = ({ key }) => key.toLowerCase().includes('vimeo');
 }
+
+const resolver = {
+  ...ContentItem.resolver,
+  UniversalContentItem: {
+    ...ContentItem.resolver.UniversalContentItem,
+    isFeatured: ({ attributeValues }) =>
+      attributeValues.isFeatured.value === 'True',
+    subtitle: ({ attributeValues }) => attributeValues.subtitle.value,
+  },
+};
 
 export { resolver, schema, dataSource };
