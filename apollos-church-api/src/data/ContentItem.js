95da7d6a45fd00dadd29d5dfa09ffb7b5865fa11
Michael Neeley <micneeley14@gmail.com>
2021-03-16 13:24:46 -0400

    add cta links (#5)
    * add cta links
    
    * images
    
    * removed log

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { ContentItem } from '@apollosproject/data-connector-rock';
+import ApollosConfig from '@apollosproject/config';
 
 const schema = gql`
   ${ContentItem.schema}
@@ -8,6 +9,15 @@ const schema = gql`
     isFeatured: Boolean
     subtitle: String
     staff: Group
+    ctaLinks: [CTA]
+  }
+
+  type CTA {
+    title: String
+    body: String
+    image: ImageMedia
+    buttonText: String
+    buttonLink: String
   }
 `;
 
@@ -28,6 +38,25 @@ const resolver = {
             .filter(`Guid eq guid'${staff?.value}'`)
             .first()
         : null,
+    ctaLinks: (
+      { attributeValues: { ctaLinks } },
+      args,
+      { dataSources: { Matrix } }
+    ) => Matrix.getItemsFromGuid(ctaLinks?.value),
+  },
+  CTA: {
+    title: ({ attributeValues: { title } }) => title?.value,
+    body: ({ attributeValues: { body } }) => body?.value,
+    image: ({ attributeValues: { image } }) => ({
+      __typename: 'ImageMedia',
+      key: image?.attributeId,
+      name: image?.value,
+      sources: image?.value
+        ? [{ uri: `${ApollosConfig.ROCK.IMAGE_URL}?guid=${image?.value}` }]
+        : [],
+    }),
+    buttonText: ({ attributeValues: { buttonText } }) => buttonText?.value,
+    buttonLink: ({ attributeValues: { buttonLink } }) => buttonLink?.value,
   },
 };
 
