3854602d394aea32fe8157569a0ca0771f5f5e89
Michael Neeley <micneeley14@gmail.com>
2021-03-29 14:17:34 -0400

    adds custom search facets

---
+++ apollos-church-api/src/data/Algolia.js
@@ -0,0 +1,34 @@
+import { graphql } from 'graphql';
+import * as baseSearch from '@apollosproject/data-connector-algolia-search';
+
+const { schema, resolver, dataSource: BaseSearch } = baseSearch;
+
+export class Search extends BaseSearch {
+  baseMapItemToAlgolia = this.mapItemToAlgolia;
+
+  async mapItemToAlgolia(item) {
+    const node = await this.baseMapItemToAlgolia(item);
+
+    const { data } = await graphql(
+      this.context.schema,
+      `
+      query GetItem {
+        node(id: "${node.id}") {
+          ... on UniversalContentItem {
+            campus { name }
+            staff { name }
+          }
+        }
+      }`,
+      {},
+      this.context
+    );
+    return {
+      ...node,
+      location: data.node.campus.name,
+      ministry: data.node.staff.name,
+    };
+  }
+}
+
+export { schema, Search as dataSource, resolver };
--- apollos-church-api/src/data/index.js
+++ apollos-church-api/src/data/index.js
@@ -10,7 +10,7 @@ import * as Scripture from '@apollosproject/data-connector-bible';
 import * as LiveStream from '@apollosproject/data-connector-church-online';
 import * as Cloudinary from '@apollosproject/data-connector-cloudinary';
 import * as OneSignal from '@apollosproject/data-connector-onesignal';
-import * as Search from '@apollosproject/data-connector-algolia-search';
+// import * as Search from '@apollosproject/data-connector-algolia-search';
 import * as Pass from '@apollosproject/data-connector-passes';
 import * as Cache from '@apollosproject/data-connector-redis-cache';
 import * as Sms from '@apollosproject/data-connector-twilio';
@@ -54,6 +54,7 @@ import * as FeatureFeed from './feature-feeds';
 import * as Vimeo from './Vimeo';
 import * as ContentItem from './ContentItem';
 import * as Matrix from './Matrix';
+import * as Search from './Algolia';
 
 // This module is used to attach Rock User updating to the OneSignal module.
 // This module includes a Resolver that overides a resolver defined in `OneSignal`
