02374d6f7e723f9f347ba4d5dab99314eecc7ddd
Michael Neeley <micneeley14@gmail.com>
2021-04-28 15:24:43 -0400

    upgrade API to Apollos 2.13.1

--- apollos-church-api/src/data/index.postgres.js
+++ apollos-church-api/src/data/index.postgres.js
@@ -7,18 +7,17 @@ import {
 
 import * as Analytics from '@apollosproject/data-connector-analytics';
 import * as Scripture from '@apollosproject/data-connector-bible';
-// import * as LiveStream from '@apollosproject/data-connector-church-online';
+import * as LiveStream from '@apollosproject/data-connector-church-online';
 import * as Cloudinary from '@apollosproject/data-connector-cloudinary';
-import * as OneSignal from '@apollosproject/data-connector-onesignal';
-// import * as Search from '@apollosproject/data-connector-algolia-search';
+import * as Search from '@apollosproject/data-connector-algolia-search';
 import * as Pass from '@apollosproject/data-connector-passes';
 import * as Cache from '@apollosproject/data-connector-redis-cache';
 import * as Sms from '@apollosproject/data-connector-twilio';
 import {
   Followings,
-  // Interactions,
+  Interactions,
   RockConstants,
-  // ContentItem,
+  ContentItem,
   ContentChannel,
   Sharable,
   Auth,
@@ -27,40 +26,39 @@ import {
   AuthSms,
   Campus,
   Group,
-  // Feature,
-  // FeatureFeed,
+  BinaryFiles,
+  Feature,
+  FeatureFeed,
   ActionAlgorithm,
   Event,
   PrayerRequest,
   Persona,
-  // Person
-  BinaryFiles,
+  Person as RockPerson,
 } from '@apollosproject/data-connector-rock';
 
+import {
+  Comment,
+  UserFlag,
+  UserLike,
+  Follow,
+  Campus as PostgresCampus,
+  Person as PostgresPerson,
+} from '@apollosproject/data-connector-postgres';
+
 import * as Theme from './theme';
-import * as Feature from './features';
-import * as FeatureFeed from './feature-feeds';
-import * as Vimeo from './Vimeo';
-import * as ContentItem from './ContentItem';
-import * as Person from './Person';
-import * as Matrix from './Matrix';
-import * as Search from './Algolia';
-import * as LiveStream from './LiveStream';
-import * as Interactions from './interactions';
 
-// This module is used to attach Rock User updating to the OneSignal module.
-// This module includes a Resolver that overides a resolver defined in `OneSignal`
-import * as OneSignalWithRock from './oneSignalWithRock';
-
-// This is to mock any postgres resolvers so we don't throw API errors for unresolved
-// typedefs
-import NoPostgres from './noPostgres';
+// This modules ties together certain updates so they occurs in both Rock and Postgres.
+// Will be eliminated in the future through an enhancement to the Shovel
+import { Person, OneSignal } from './rockWithPostgres';
 
 const data = {
   Interfaces,
   Followings,
   ContentChannel,
   ContentItem,
+  RockPerson, // This entry needs to come before (postgres) Person
+  BinaryFiles, // This entry needs to come before (postgres) Person
+  PostgresPerson, // Postgres person for now, as we extend this dataSource in the 'rockWithPostgres' file
   Cloudinary,
   Auth,
   AuthSms,
@@ -74,7 +72,6 @@ const data = {
   Analytics,
   OneSignal,
   PersonalDevice,
-  OneSignalWithRock,
   Pass,
   Search,
   Template,
@@ -86,12 +83,13 @@ const data = {
   Event,
   Cache,
   PrayerRequest,
-  Vimeo,
+  Comment,
+  UserLike,
+  UserFlag,
+  Follow,
+  PostgresCampus,
   Persona,
-  Matrix,
-  Person,
-  BinaryFiles,
-  NoPostgres,
+  Person, // An extension of Postgres person. Will be eliminated in the near future so you can use just postgres/Person.
 };
 
 const {
