8df2d3b2c913efa9f45be54ca6549e2578fe9860
Michael Neeley <micneeley14@gmail.com>
2021-05-24 14:01:39 -0400

    sort children by order

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -104,49 +104,22 @@ class dataSource extends ContentItem.dataSource {
         ({ childContentChannelItemId }) => childContentChannelItemId
       )
     );
-    let sortByOrder = false;
 
-    // If this is true, our transform will be activated.
-    // The transform is the only way to sort by `order`
-    const originalOrderBy = cursor.orderBy;
-
-    // Here we hijack the default behavior of the `.order` call, using it to set
-    // a value that we are going to use to trigger the `transform`
-    cursor.orderBy = (name, direction) => {
-      if (name === 'Order') {
-        sortByOrder = true;
-      }
-      // call is important here to preserve the original value of `this` (it should be `cursor`)
-      return originalOrderBy.call(cursor, name, direction);
-    };
-
-    // Same as above.
-    const originalSort = cursor.sort;
-    cursor.sort = (sorts) => {
-      if (sorts.map(({ field }) => field).includes('Order')) {
-        sortByOrder = true;
-      }
-      return originalSort.call(cursor, sorts);
-    };
-
-    cursor.transform((results) => {
-      // If we have called .orderBy or .sort w/ `order`, we do our in memory sort.
-      if (sortByOrder) {
-        return results.sort((a, b) => {
-          /**
-           * Find the Association Order for the given content channel items
-           */
-          const { order: orderA } = associations.find(
-            (item) => item.childContentChannelItemId === a.id
-          );
-          const { order: orderB } = associations.find(
-            (item) => item.childContentChannelItemId === b.id
-          );
-          return orderA - orderB;
-        });
-      }
-      return results;
-    });
+    cursor.transform((results) =>
+      // sort by order
+      results.sort((a, b) => {
+        /**
+         * Find the Association Order for the given content channel items
+         */
+        const { order: orderA } = associations.find(
+          (item) => item.childContentChannelItemId === a.id
+        );
+        const { order: orderB } = associations.find(
+          (item) => item.childContentChannelItemId === b.id
+        );
+        return orderA - orderB;
+      })
+    );
 
     // Return the cursor.
     return cursor;
