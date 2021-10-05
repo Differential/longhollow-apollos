513b6e738051402b192ee5614b6d6b22e0b343e4
nlewis84 <nathan@differential.com>
2021-10-05 11:28:50 -0500

    more initial setup

---
+++ apollos-church-api/src/data/ActionAlgorithm.js
@@ -0,0 +1,57 @@
+import { get } from 'lodash';
+
+import { ActionAlgorithm } from '@apollosproject/data-connector-rock';
+
+const { resolver } = ActionAlgorithm;
+
+class dataSource extends ActionAlgorithm.dataSource {
+  ACTION_ALGORITHMS = {
+    ...this.ACTION_ALGORITHMS,
+    PRIORITY_CONTENT_FEED: this.priorityContentFeedAlgorithm.bind(this),
+  };
+
+  async priorityContentFeedAlgorithm({
+    channelIds = [],
+    limit = 20,
+    skip = 0,
+  } = {}) {
+    const { ContentItem } = this.context.dataSources;
+
+    const items = await ContentItem.byContentChannelIds(channelIds)
+      .top(limit)
+      .skip(skip)
+      .get();
+
+    return items.map((item, i) => ({
+      id: `${item.id}${i}`,
+      title: item.title,
+      subtitle: get(item, 'contentChannel.name'),
+      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+
+//   async taggedContentFeedAlgorithm({ tag, limit = 20, skip = 0 } = {}) {
+//     const { ContentItem } = this.context.dataSources;
+
+//     const cursor = await ContentItem.byTaggedContent(tag);
+//     const items = await cursor
+//       .top(limit)
+//       .skip(skip)
+//       .get();
+
+//     return items.map((item, i) => ({
+//       id: `${item.id}${i}`,
+//       title: item.title,
+//       subtitle: get(item, 'contentChannel.name'),
+//       relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+//       image: ContentItem.getCoverImage(item),
+//       action: 'READ_CONTENT',
+//       summary: ContentItem.createSummary(item),
+//     }));
+//   }
+}
+
+export { resolver, dataSource };
