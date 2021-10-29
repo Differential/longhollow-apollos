56d8340b7be9851f316dec93f5f72b86c475a013
nlewis84 <nathan@differential.com>
2021-10-29 15:22:08 -0500

    initial work

--- apollos-church-api/src/data/ActionAlgorithm.js
+++ apollos-church-api/src/data/ActionAlgorithm.js
@@ -7,6 +7,7 @@ class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
     HOME_TAB_CONTENT_FEED: this.homeTabContentFeedAlgorithm.bind(this),
+    NON_PERSONA_CONTENT_FEED: this.nonPersonaContentFeedAlgorithm.bind(this),
   };
 
   async homeTabContentFeedAlgorithm({ channelIds = [], limit = 40, skip = 0 } = {}) {
@@ -31,6 +32,25 @@ class dataSource extends ActionAlgorithm.dataSource {
     );
 
     return combinedFeaturedAndShownItems.map((item, i) => ({
+      id: `${item.id}${i}`,
+      title: item.title,
+      subtitle: get(item, 'contentChannel.name'),
+      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+
+  async nonPersonaContentFeedAlgorithm({ channelIds = [], limit = 40, skip = 0 } = {}) {
+    const { ContentItem } = this.context.dataSources;
+
+    const items = await ContentItem.byContentChannelIds(channelIds)
+      .top(limit)
+      .skip(skip)
+      .get();
+
+    return items.map((item, i) => ({
       id: `${item.id}${i}`,
       title: item.title,
       subtitle: get(item, 'contentChannel.name'),
