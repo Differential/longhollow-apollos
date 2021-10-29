01b120d951c35f70b0cc7c93b4ea539cb3fc2f5a
Nathan Lewis <nathan@differential.com>
2021-10-29 14:31:19 -0500

    feat: new home feed algo (#41)
    * initial work
    
    * new homeFeed algo
    
    * added back channels that were removed for clarity of testing

---
+++ apollos-church-api/src/data/ActionAlgorithm.js
@@ -0,0 +1,45 @@
+import { get } from 'lodash';
+
+import { ActionAlgorithm } from '@apollosproject/data-connector-rock';
+
+const { resolver, schema } = ActionAlgorithm;
+class dataSource extends ActionAlgorithm.dataSource {
+  ACTION_ALGORITHMS = {
+    ...this.ACTION_ALGORITHMS,
+    HOME_TAB_CONTENT_FEED: this.homeTabContentFeedAlgorithm.bind(this),
+  };
+
+  async homeTabContentFeedAlgorithm({ channelIds = [], limit = 40, skip = 0 } = {}) {
+    const { ContentItem } = this.context.dataSources;
+
+    const items = await ContentItem.byContentChannelIds(channelIds)
+      .top(limit)
+      .skip(skip)
+      .get();
+
+    const featuredItems = items.filter(
+      (item) => item.attributeValues.featuredonHomePage.value === 'True'
+    );
+
+    const shownOnHomeItems = items.filter(
+      (item) => item.attributeValues.shownonHomePage.value === 'True'
+    );
+
+    // This returns the featured item first, then the first 2 items marked to be shown on the Home Feed
+    const combinedFeaturedAndShownItems = featuredItems.concat(
+      shownOnHomeItems.slice(0, 2)
+    );
+
+    return combinedFeaturedAndShownItems.map((item, i) => ({
+      id: `${item.id}${i}`,
+      title: item.title,
+      subtitle: get(item, 'contentChannel.name'),
+      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+}
+
+export { schema, resolver, dataSource };
