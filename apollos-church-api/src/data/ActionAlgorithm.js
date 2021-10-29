d307077b407855bb8beb5ceff2334f49cd878782
nlewis84 <nathan@differential.com>
2021-10-29 09:40:40 -0500

    prelim work

--- apollos-church-api/src/data/ActionAlgorithm.js
+++ apollos-church-api/src/data/ActionAlgorithm.js
@@ -7,21 +7,18 @@ const { resolver } = ActionAlgorithm;
 class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
-    PRIORITY_CONTENT_FEED: this.priorityContentFeedAlgorithm.bind(this),
+    HOME_TAB_CONTENT_FEED: this.homeTabContentFeedAlgorithm.bind(this),
   };
 
-  async priorityContentFeedAlgorithm({
-    channelIds = [],
-    limit = 20,
-    skip = 0,
-  } = {}) {
+  async homeTabContentFeedAlgorithm({ channelIds = [], limit = 40, skip = 0 } = {}) {
     const { ContentItem } = this.context.dataSources;
 
     const items = await ContentItem.byContentChannelIds(channelIds)
       .top(limit)
       .skip(skip)
       .get();
-
+console.log('items: ', items[0])
+// filter out by shownon home page here featured on home page
     return items.map((item, i) => ({
       id: `${item.id}${i}`,
       title: item.title,
@@ -32,26 +29,6 @@ class dataSource extends ActionAlgorithm.dataSource {
       summary: ContentItem.createSummary(item),
     }));
   }
-
-//   async taggedContentFeedAlgorithm({ tag, limit = 20, skip = 0 } = {}) {
-//     const { ContentItem } = this.context.dataSources;
-
-//     const cursor = await ContentItem.byTaggedContent(tag);
-//     const items = await cursor
-//       .top(limit)
-//       .skip(skip)
-//       .get();
-
-//     return items.map((item, i) => ({
-//       id: `${item.id}${i}`,
-//       title: item.title,
-//       subtitle: get(item, 'contentChannel.name'),
-//       relatedNode: { ...item, __type: ContentItem.resolveType(item) },
-//       image: ContentItem.getCoverImage(item),
-//       action: 'READ_CONTENT',
-//       summary: ContentItem.createSummary(item),
-//     }));
-//   }
 }
 
 export { resolver, dataSource };
