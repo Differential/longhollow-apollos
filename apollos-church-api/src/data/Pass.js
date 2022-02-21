4e4d73da9060e2fd40161cd16130d6279ae13f37
Conrad VanLandingham <conrad@differential.com>
2022-02-21 11:20:27 -0600

    add custom passkit url

--- apollos-church-api/src/data/Pass.js
+++ apollos-church-api/src/data/Pass.js
@@ -8,7 +8,11 @@ const resolver = {
   ...baseResolver,
   Pass: {
     ...baseResolver.Pass,
-    passkitFileUrl: () => null,
+    passkitFileUrl: async (_, __, { dataSources }) => {
+      // check for this first because the next data call takes a long time
+      const authToken = await dataSources.Auth.getAuthToken();
+      return `https://rock.longhollow.com/page/1308?rckipid=${authToken}`;
+    },
   },
 };
 
