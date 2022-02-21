dfc27710d02f6d09354b98ec353d67a1db7bc3d8
Conrad VanLandingham <conrad@differential.com>
2022-02-21 16:06:02 -0600

    use in app browser for wallet pass from rock

--- apollos-church-api/src/data/Pass.js
+++ apollos-church-api/src/data/Pass.js
@@ -8,11 +8,7 @@ const resolver = {
   ...baseResolver,
   Pass: {
     ...baseResolver.Pass,
-    passkitFileUrl: async (_, __, { dataSources }) => {
-      // check for this first because the next data call takes a long time
-      const authToken = await dataSources.Auth.getAuthToken();
-      return `https://rock.longhollow.com/page/1308?rckipid=${authToken}`;
-    },
+    passkitFileUrl: () => null,
   },
 };
 
--- apolloschurchapp/src/tabs/connect/ActionBar.js
+++ apolloschurchapp/src/tabs/connect/ActionBar.js
@@ -1,63 +1,33 @@
 import React from 'react';
-import { Query } from '@apollo/client/react/components';
-import gql from 'graphql-tag';
 import { ActionBar, ActionBarItem } from '@apollosproject/ui-kit';
 import { RockAuthedWebBrowser } from '@apollosproject/ui-connected';
-import { useNavigation } from '@react-navigation/native';
 
-const Toolbar = () => {
-  const navigation = useNavigation();
-  return (
-    <RockAuthedWebBrowser>
-      {(openUrl) => (
-        <ActionBar>
-          <ActionBarItem
-            onPress={() => openUrl('https://www.longhollow.com/about/schedule')}
-            icon="time"
-            label="Schedule"
-          />
-          <Query
-            query={gql`
-              {
-                currentUser {
-                  id
-                  profile {
-                    id
-                    groups {
-                      id
-                      name
-                    }
-                  }
-                }
-              }
-            `}
-            fetch-policy={'cache-and-network'}
-          >
-            {({ data, loading, error }) => {
-              if (loading) return null;
-              if (error) return null;
-              return data.currentUser.profile.groups.length ? (
-                <ActionBarItem
-                  onPress={() => navigation.navigate('Passes')}
-                  icon="check"
-                  label="Check-in"
-                />
-              ) : null;
-            }}
-          </Query>
-          <ActionBarItem
-            onPress={() =>
-              openUrl('https://www.longhollow.com/online-giving', {
-                externalBrowser: true,
-              })
-            }
-            icon="heart-solid"
-            label="Give"
-          />
-        </ActionBar>
-      )}
-    </RockAuthedWebBrowser>
-  );
-};
+const Toolbar = () => (
+  <RockAuthedWebBrowser>
+    {(openUrl) => (
+      <ActionBar>
+        <ActionBarItem
+          onPress={() => openUrl('https://www.longhollow.com/about/schedule')}
+          icon="time"
+          label="Schedule"
+        />
+        <ActionBarItem
+          onPress={() => openUrl('https://rock.longhollow.com/page/1308')}
+          icon="check"
+          label="Check-in"
+        />
+        <ActionBarItem
+          onPress={() =>
+            openUrl('https://www.longhollow.com/online-giving', {
+              externalBrowser: true,
+            })
+          }
+          icon="heart-solid"
+          label="Give"
+        />
+      </ActionBar>
+    )}
+  </RockAuthedWebBrowser>
+);
 
 export default Toolbar;
